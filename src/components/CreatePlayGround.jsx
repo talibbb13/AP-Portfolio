c5cd33e6d572a94f2143d497c88e2d5c66ed2aeb
Mohd Talib <103596639+talibbb13@users.noreply.github.com>
2024-06-19 02:07:01 +0530

    Update CreatePlayGround.jsx
    Iris Movement Feature Added

--- src/components/CreatePlayGround.jsx
+++ src/components/CreatePlayGround.jsx
@@ -2,14 +2,33 @@ import { useEffect, useState } from "react";
 
 function CreatePlayGround({ bgElement, video, otherElements }) {
   const [rotate, setRotate] = useState(0);
+{/* ummmm */}
+const [transX, setTransX] = useState(0)
+const [transY, setTransY] = useState(0)
 
   useEffect(() => {
+{/* experiment area */}
+let translateX = 0
+let translateY = 0
+let prevX = 0
+let prevY = 0
+
+
     window.addEventListener("mousemove", (e) => {
       let deltaX = e.clientX - window.innerWidth / 2;
       let deltaY = e.clientY - window.innerHeight / 2;
 
       const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
       setRotate(angle);
+
+{/* again... */}
+var xDiff = e.clientX - prevX
+var yDiff = e.clinetY - prevY
+translateX = gsap.utils.clamp(-10, 10, xDiff)
+translateY = gsap.utils.clamp(-10, 10, yDiff)
+setTransX(translateX)
+setTransY(translateY)
+
     });
   }, []);
 
@@ -25,7 +44,9 @@ function CreatePlayGround({ bgElement, video, otherElements }) {
 
       <div className="Eyes z-50 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center gap-8">
         <div className="Sclera h-[13vw] w-[13vw] bg-zinc-200 rounded-full flex justify-center items-center">
-          <div className="Iris relative h-1/2 w-1/2 rounded-full bg-black hover:bg-zinc-900">
+
+{/* boommmm */ }
+          <div className="Iris relative h-1/2 w-1/2 rounded-full bg-black hover:bg-zinc-900" style={{transform: `translate(${transX}px, ${transY}px)`}}>
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-xs">
               Play
             </div>
