78652f879ca77657f762baad259cb5ed9fd5b942
Talib Raeen <talibraeen13@gmail.com>
2024-06-22 20:07:59 +0530

    skill page created

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,11 +1,129 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
+  const mySkills = [
+    {
+      title: "Html",
+      imgSrc:
+        "https://static-00.iconduck.com/assets.00/html5-icon-2018x2048-st7q7lm6.png",
+      content:
+        "I have a strong foundation in HTML, allowing me to create well-structured and semantic web pages.",
+      link: "https://talibbb13.github.io/SP-Flash-Cards/",
+    },
+    {
+      title: "CSS",
+      imgSrc:
+        "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png",
+      content:
+        "With CSS, I bring designs to life. I can create visually appealing and responsive layouts, making sure websites look great on any device.",
+      link: "https://talibbb13.github.io/BP-Apni-Kaksha/",
+    },
+    {
+      title: "JavaScript",
+      imgSrc:
+        "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+      content:
+        "JavaScript is my go-to language for adding interactivity to web pages. From simple scripts to complex applications, I can handle it all.",
+      link: "https://talibbb13.github.io/BP-Bubble-Game/",
+    },
+    {
+      title: "Tailwind CSS",
+      imgSrc:
+        "https://seeklogo.com/images/T/tailwind-css-logo-5AD4175897-seeklogo.com.png",
+      content:
+        "I use Tailwind CSS to rapidly build custom user interfaces. Its utility-first approach helps me create reusable styles efficiently.",
+      link: "https://talibbb13.github.io/React-Change-BG-Color",
+    },
+    {
+      title: "Bootstrap",
+      imgSrc:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png",
+      content:
+        "Bootstrap allows me to quickly develop responsive and modern websites. I leverage its pre-built components for development process.",
+      link: "https://talibbb13.github.io/BP-iCoder/",
+    },
+    {
+      title: "React",
+      imgSrc:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+      content:
+        "React is my most library for building dynamic user interfaces. I have experience with Redux, Context API, and hooks to manage state and enhance performance.",
+      link: "https://talibbb13.github.io/React-Password-Gen/",
+    },
+    {
+      title: "Appwrite & React Hook Form",
+      imgSrc: "https://appwrite.io/assets/logomark/logo.png",
+      content:
+        "I use Appwrite for backend services and React Hook Form for managing form state in React. This combination allows me to build robust and scalable applications.",
+      link: "https://anonymous-blog-web-app.vercel.app",
+    },
+    {
+      title: "Git",
+      imgSrc:
+        "https://cdn.iconscout.com/icon/free/png-256/free-git-19-1175220.png",
+      content:
+        "Git is essential for version control in my projects. I am proficient in using Git for collaboration, code management, and maintaining a clean project.",
+      link: "https://github.com/talibbb13",
+    },
+    {
+      title: "GSAP",
+      imgSrc: "https://cdn.worldvectorlogo.com/logos/gsap-greensock.svg",
+      content:
+        "With GSAP, I create high-performance animations that enhance the user experience. It helps me bring web pages to life with smooth and engaging transitions.",
+      link: "https://talibbb13.github.io/GSAP-React-Buzz-Around/",
+    },
+    {
+      title: "Locomotive",
+      imgSrc:
+        "https://lh3.googleusercontent.com/pw/AP1GczM_h18XK51vpj2jh1LQz7MtD-RbZa1cJyH8fLVn5wHJpjlMMU98_V_yy8ON9MP4a4Bg4jWr8cHndgQX7r2-oP_t8uz80CIqVMvsT8P_2SX_9o9hOLYUsIN5rUClvcdQcRj924SIjEKC33EHKclUrec1dTXigyDylF4DrETl6NmvVbT9eJCt56qeC-ds-iFdiw37Aa0OPnqP5jl0GS9SIxn_Cf2HCWfANkiRIUAKxSfrlUzpCyYvlhDs2wztnP8NOr4ikvPjfNzsDZ0fQp7TjNcAV8PtbTZjdLrRIQ3AmXamD_UWB4YxscSub7MAEL8GhVtD-mXcpLt_ygwl54uGEyGUP_eJwMovYZaTWPa9R3fH4g1ilHtW5PlKmot8NK8bkXUIlpImQwTvwKWu-fUm59IZMNJA09ErW-eTKgifiG9xu9tKQQHPu98ZkAPqagH5Lowf1AWPfIwrNspJQxYnV-typbdl_Bkbmi1mGLQ9rAxo9gRHHlmSWpOEFsFSzf3V0hUH17l5e0vDL9LM3E3JSuK0gbO9pzxPGM4GtJO7o3jae35SAtYYz89UpeMqfMpusqq4Wv_DG0JszliKubcrfbS3sMMWaMXHnqbTnyNlo5NzBXVGXTPw-Ij7ZSI0O5SFpHLvtTnutxqmPQ-THuIrPrJD1WK9a677y4fV8oovsAEbI8ngErF9EB2LK0uxFJm99K4OD2ibuoL51jC4fv-om10pWAmKeL2qvw-3-NFfMwUwlrC2gzAJkzNOJsm7uieuySxTPPhSvR4VReH6PrP4oqvCcrPolXFDKJySj1A862FttwtMKfXAM5HaTssV6XYyEQYSIHOi2kldFbTgS30Kd7gjvDgwz_8Vjcfk5nt1ioh_NHf5p4rCvLapCksa-ViX73_jSrs0ZCgWLIvxRk1cuJRdpkXSr7hStBKFEdTEDaIZ4zS3iygoTEIa19A=w469-h532-s-no-gm?authuser=0",
+      content:
+        "Locomotive Scroll allows me to implement smooth scrolling, adding a modern touch to web pages and improving user interaction.",
+      link: "/",
+    },
+    {
+      title: "Excel",
+      imgSrc:
+        "https://cdn.pixabay.com/photo/2023/06/01/12/02/excel-logo-8033473_640.png",
+      content:
+        "I can also use Microsoft Excel. My skills include creating complex spreadsheets, data visualization, and using formulas to extract insights.",
+      link: "https://drive.google.com/drive/folders/1-1TKSZ4jVOjNC9NLAtC_fp_a27-Vx_rz?usp=drive_link",
+    },
+  ];
+
   return (
-    <div className="h-screen w-full flex justify-center items-center">
-      Skills
+    <div className="bg-[#0F0F0F] text-white pt-32 px-14">
+      <h1 className="Founder leading-[.75] -tracking-[.01em] text-8xl font-bold mb-8 text-center uppercase">
+        I can Offer you...
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {mySkills.map((skill, ind) => (
+          <div
+            key={ind}
+            className="group bg-[#014D43] p-6 rounded-lg shadow-lg hover:bg-[#CDEA68] hover:text-black transition duration-300 hover:scale-95"
+          >
+            <h2 className="Grotesk text-2xl font-semibold mb-4">
+              {skill.title}
+            </h2>
+            <div className="flex justify-center items-center mb-7">
+              <img
+                src={skill.imgSrc}
+                alt={skill.title}
+                className="group-hover:scale-125 max-h-40 object-contain rounded-md transition duration-300"
+              />
+            </div>
+            <p className="mb-4">{skill.content}</p>
+            <a
+              href={skill.link}
+              target="_blank"
+              className="bg-[#CDEA68] text-[#014D43] py-2 px-4 rounded-lg hover:bg-[#014D43] hover:text-[#CDEA68] transition duration-300"
+            >
+              View It
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default Skills
+export default Skills;
